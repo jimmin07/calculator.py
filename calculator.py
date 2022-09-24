ee3f27d40d98d63276b059cf4cae370bcb21f255
jimin <103527488+jimmin07@users.noreply.github.com>
2022-09-25 06:12:11 +0900

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,9 +6,9 @@ calculate = input("연산 선택(+, -, *, /): ")
 
 def add(x, y):
     return x + y
-def subtract(x, y):
+def sub(x, y):
     return x - y
-def multiply(x, y):
+def mult(x, y):
     return x * y
 def divide(x, y):
     return x / y
@@ -17,10 +17,10 @@ if calculate == '+':
     print(add(a, b))
 
 elif calculate == '-':
-    print(subtract(a, b))
+    print(sub(a, b))
 
 elif calculate == '*':
-    print(multiply(a, b))
+    print(mult(a, b))
 
 elif calculate == '/':
     print(divide(a, b))
